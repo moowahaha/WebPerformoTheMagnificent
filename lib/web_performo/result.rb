315ddb4c06d4046ed4bf618cd529f0f72f7084fe
Stephen Hardisty <shardisty@jhart0506.win.int.realestate.com.au>
2010-05-11 14:44:42 +1000

    Added result comparison tests & code

--- lib/web_performo/result.rb
+++ lib/web_performo/result.rb
@@ -5,5 +5,13 @@ class WebPerformo
     def initialize result
       @url, @first_byte_speed, @render_speed = result[:url], result[:first_byte_speed], result[:render_speed]
     end
+
+    def > candidate_result
+      self.first_byte_speed > candidate_result.first_byte_speed && self.render_speed > candidate_result.render_speed
+    end
+
+    def < candidate_result
+      self.first_byte_speed < candidate_result.first_byte_speed && self.render_speed < candidate_result.render_speed
+    end
   end
 end
---
+++ spec/web_performo_results_spec.rb
@@ -0,0 +1,26 @@
+require File.join(File.dirname(__FILE__), '..', 'lib', 'web_performo', 'result')
+
+describe WebPerformo::Result do
+  it "should allow us to fetch data we sat" do
+    result = WebPerformo::Result.new(:render_speed => 10, :first_byte_speed => 20, :url => 'bob')
+
+    result.render_speed.should == 10
+    result.first_byte_speed.should == 20
+    result.url.should == 'bob'
+  end
+
+  describe 'comparing' do
+    before do
+      @slow_result = WebPerformo::Result.new(:render_speed => 2, :first_byte_speed => 2, :url => 'slow')
+      @fast_result = WebPerformo::Result.new(:render_speed => 1, :first_byte_speed => 1, :url => 'fast')
+    end
+
+    it "should recognise results that are faster" do
+      @fast_result.should < @slow_result
+    end
+
+    it "should recognise results that are slow" do
+      @slow_result.should > @fast_result
+    end
+  end
+end
